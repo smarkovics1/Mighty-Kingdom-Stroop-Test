d7fcbdcaca77006c26d2892d968c9d75d3fcfcb6
smarkovics <smarkovics00@gmail.com>
2021-12-11 17:34:19 +1000

    created Gamecontroller script and create button variables

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+
+    [Header("Answer Buttons")]
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+
+    public TextMeshProUGUI mainText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
